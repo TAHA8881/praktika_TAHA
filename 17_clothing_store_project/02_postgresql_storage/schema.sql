ffe056aea49111ac33339cf5af67bbd11f73045e
TAHA8881 <shaprina.tatiana888@gmail.com>
2026-06-21 17:34:53 +0300

    проверила этап 2 дома

--- 17_clothing_store_project/02_postgresql_storage/schema.sql
+++ 17_clothing_store_project/02_postgresql_storage/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE products (
     price INTEGER NOT NULL CHECK (price >= 0),
     color VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
-    is_active boolean not null
+    is_active BOOLEAN NOT NULL
 );
 
 -- TODO: таблица остатков товаров по размерам
@@ -31,35 +31,54 @@ CREATE TABLE leftsizes (
 );
 
 -- TODO: таблица покупателей
+
 CREATE TABLE customers (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    email TEXT NOT NULL CHEK(email != '@'),
+    email TEXT NOT NULL CHECK (email LIKE '%@%'),
 	phone TEXT NOT NULL
 );
 
 -- Следующие таблицы понадобятся на следующих этапах проекта.
 
+-- TODO: таблица промокодов
+
+CREATE TABLE promocodes (
+    code VARCHAR(50) PRIMARY KEY,
+    percent INTEGER NOT NULL CHECK (percent BETWEEN 0 AND 100),
+    min_total INTEGER NOT NULL CHECK (min_total >= 0),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- TODO: таблица адресов доставки
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES customers(id),
+    address TEXT NOT NULL,
+    is_default BOOLEAN DEFAULT FALSE
+);
+
 -- TODO: таблица заказов
+
 CREATE TABLE orders (
     id INTEGER PRIMARY KEY,
-    users_id INTEGER NOT NULL REFERENCES users(id),
-    total_price NUMERIC(8,2) NOT NULL CHEK(total_price >= 0),
-	status TEXT NOT NULL
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    customer_id INTEGER NOT NULL REFERENCES customers(id),
+    total_original INTEGER NOT NULL CHECK (total_original >= 0),
+    discount INTEGER NOT NULL DEFAULT 0 CHECK (discount >= 0),
+    total_final INTEGER NOT NULL CHECK (total_final >= 0),
+    status TEXT NOT NULL DEFAULT 'создан',
+    promocode_used VARCHAR(50) REFERENCES promocodes(code),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 -- TODO: таблица позиций заказа
 
 CREATE TABLE order_items (
     id INTEGER PRIMARY KEY,
-    order_id INTEGER NOT NULL REFERENCES order(id),
-    clothes_id INTEGER NOT NULL REFERENCES order(id),
+    order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    clothes_id INTEGER NOT NULL REFERENCES products(id),
     clothes_name TEXT NOT NULL,
 	size TEXT NOT NULL,
-    price NUMERIC(8,2) NOT NULL CHECK(price > 0)
-    quantity INTEGER NOT NULL CHECK(quantity > 0),
+    price NUMERIC(8,2) NOT NULL CHECK(price > 0),
+    quantity INTEGER NOT NULL CHECK(quantity > 0)
 );
--- TODO: таблица адресов доставки
-
--- TODO: таблица промокодов
-
